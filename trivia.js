5f82970d0bb063204ea902b02207654d20c057ba
kmeitz <54551992+kmeitz@users.noreply.github.com>
2019-10-16 17:11:49 -0500

    Correct Data format

--- trivia.js
+++ trivia.js
@@ -1,11 +1,14 @@
 console.log('starting');
 const api = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=boolean';
 
-$(document).ready(() =>  {
-    $("#startButton").click(() => {
-        $("#div1").load(api);
+$(document).ready(function(){
+  $("button").click(function(){
+    $.ajax({url: api, success: function(result){
+      $("#div1").html(result);
+      console.log(`result: ${JSON.stringify(result, null, 3)}`);
+    }});
   });
 });
-    
+
     
     
