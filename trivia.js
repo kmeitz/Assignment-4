73180aed19f35e8e5a4540fde823890cfa0b9721
kmeitz <54551992+kmeitz@users.noreply.github.com>
2019-10-16 18:54:03 -0500

    Hiding start button and showing submit

--- trivia.js
+++ trivia.js
@@ -1,14 +1,22 @@
 console.log('starting');
+$("#submitButton").hide();
 const api = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=boolean';
 
 $(document).ready(function(){
-  $("button").click(function(){
+  $("#startButton").click(function(){
     $.ajax({url: api, success: function(result){
       $("#div1").html(result);
       console.log(`result: ${JSON.stringify(result, null, 3)}`);
+      data = `result: ${JSON.stringify(result, null, 3)}`
+      
+      $("#startButton").hide();
     }});
   });
 });
+$("#submitButton").show();
+//for (i = 0; i < 10; i++) {
+  //$("#div1").html(result[i]);
 
+  
     
     
