a29f7c1197a450f13ecfe76e8e53b355e5b640ab
kmeitz <54551992+kmeitz@users.noreply.github.com>
2019-10-16 22:50:29 -0500

    Assigning trivia variable

--- trivia.js
+++ trivia.js
@@ -1,19 +1,22 @@
 console.log('starting');
-$("#submitButton").hide();
+$(`#submitButton`).hide();
 const api = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=boolean';
 
 $(document).ready(function(){
+  
   $("#startButton").click(function(){
     $.ajax({url: api, success: function(result){
       $("#div1").html(result);
       console.log(`result: ${JSON.stringify(result, null, 3)}`);
-      data = `result: ${JSON.stringify(result, null, 3)}`
+      const trivia = `result: ${JSON.stringify(result, null, 3)}`
       
       $("#startButton").hide();
     }});
   });
 });
 $("#submitButton").show();
+
+
 //for (i = 0; i < 10; i++) {
   //$("#div1").html(result[i]);
 
