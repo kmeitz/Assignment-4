f147650fcd0f61a36993264a2fe1f511fc286299
Kristjan Meitz <kcmeit16@smumn.edu>
2019-10-18 00:41:47 -0500

    Final

--- trivia.js
+++ trivia.js
@@ -5,9 +5,17 @@ $("#restartButton").hide();
 $("#startButton").hide();
 $("#nextButton").hide();
 
-var trivia;
+var score = 0; 
+var questionNum = 0;
+var first = 1;
 
-
+$("#score").html(score);
+nbutton = $("#nextButton");
+qSpace = $("#div1");
+scoreS = $("#score");
+rbutton = $("#restartButton"); 
+fbutton = $("#falseButton") ;
+tbutton = $("#trueButton");
 
 
 
@@ -24,37 +32,99 @@ $().ready(() => {
     }).then((data) => {
       
       trivia = console.log(JSON.stringify(data,null,3))
-      const questionNum = 1;
-      const score  = 0; 
+      
       $("#trueButton").show();
-      tbutton = $("#trueButton");
+      
 $("#falseButton").show();
-fbutton = $("#falseButton") ;
-$("#restartButton").show();
-rbutton = $("#restartButton"); 
-$("#nextButton").show();
-nbutton = $("#nextButton");
-qSpace = $("#div1");
-scoreS = $("#score");
 
-$("#div1").html("Question " + questionNum + ": " + data.results[0].question);
+$("#restartButton").show();
+
+
+
+
+
+$("#div1").html("Question " + first + ": " + data.results[questionNum].question);
 
 // Restart button function
 rbutton.click(function restart() {
-  const questionNum = 0;
-  const score = 0;
-  scoreS.html("");
-  fbutton.hide();
-  tbutton.hide();
-  nbutton.hide();
-  rbutton.hide();
-  qSpace.html("");
+  $("#trueButton").hide();
+  $("#falseButton").hide();
+  $("#restartButton").hide();
+  $("#startButton").hide();
+  $("#nextButton").hide();
+  
+  score = 0; 
+  questionNum = 0;
+  first = 1;
+  scoreS.html(score);
+  qSpace.html("")
+  
   $("#questionButton").show();
-
-
 });
 
+
+
+if ( questionNum == 10) {
+  qSpace.html("Your final score is " + score + ". Refresh the page to play again");
+  tbutton.hide();
+  fbutton.hide();
+  nbutton.hide();
+}
+else { // If the true button is clicked"
+  tbutton.click(function TrueClick() {
+    $("#nextButton").show();
+    fbutton.hide();
+    tbutton.hide();
   
+    
+    
+    $("#div1").html("Question " + first + ": " + " The correct answer is " + data.results[questionNum].correct_answer + ".");
+    
+    if (data.results[questionNum].correct_answer == `True` ) {
+      score += 1;
+      scoreS.html(score);
+    }
+    questionNum += 1;
+  first += 1;
+});
+// When the next button is clicked.
+nbutton.click(function NextClick() {
+ if (questionNum == 10)
+ {
+  qSpace.html("Your final score is " + score + ". Refresh the page to play again!");
+  tbutton.hide();
+  fbutton.hide();
+  nbutton.hide();
+ }
+ else {
+  tbutton.show();
+  fbutton.show();
+  nbutton.hide();
+  $("#div1").html("Question " + first + ": " + data.results[questionNum].question);
+ }
+}); // when the false button is clicked
+fbutton.click(function FalseClick() {
+     
+  $("#nextButton").show();
+  fbutton.hide();
+  tbutton.hide();
+  
+  $("#div1").html("Question " + first + ": " + " The correct answer is " + data.results[questionNum].correct_answer + ".");
+  
+  if (data.results[questionNum].correct_answer == `False` ) {
+    score += 1;
+    scoreS.html(score);
+  }
+  questionNum += 1;
+  first += 1;
+});
+
+
+}; 
+  
+
+
+
 
 
     }); 
@@ -68,42 +138,6 @@ rbutton.click(function restart() {
 
 
 
-
-//$("#startButton").click(function start() {
-//$("#startButton").hide();
-
-
-//$("#div2").html(data.question[0]);
-//});
-
-
-//$("#trueButton").click(function trueC() {
-  //if( data.correct_answer = "True") {
-    //questionNum += 1;
-    //score += 1;
-    //$("#score").html('Score: ' + score);
-  //}
-//}
-
-
-
-  //$("#startButton").click(function(foo){
-    //$.ajax({url: api, success: function(result){
-      //$("#div1").html(result);
-      //console.log(`result: ${JSON.stringify(result, null, 3)}`);
-      //const trivia = `result: ${JSON.stringify(result, null, 3)}`
-      
-      //$("#startButton").hide();
-      //$("#trueButton").show();
-      //$("#falseButton").show();
-      //$("#restartButton").show();
-      
-      //$("#question").val(triviaData);
-      
-
-   // }});
- // });
-// });
 
 
 
