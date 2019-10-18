4355d2b475a2418edd689b7d16dec6c612386b83
Kristjan Meitz <kcmeit16@smumn.edu>
2019-10-17 21:56:33 -0500

    Bunch of Stuff

--- trivia.js
+++ trivia.js
@@ -1,24 +1,115 @@
 console.log('starting');
-$(`#submitButton`).hide();
+$("#trueButton").hide();
+$("#falseButton").hide();
+$("#restartButton").hide();
+$("#startButton").hide();
+$("#nextButton").hide();
+
+var trivia;
+
+
+
+
+
 const api = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=boolean';
 
-$(document).ready(function(){
-  
-  $("#startButton").click(function(){
-    $.ajax({url: api, success: function(result){
-      $("#div1").html(result);
-      console.log(`result: ${JSON.stringify(result, null, 3)}`);
-      const trivia = `result: ${JSON.stringify(result, null, 3)}`
+$().ready(() => { 
+  $("#questionButton").click(() => {
+    $("#questionButton").hide();
+    
+
+
+    $.ajax({
+      url: api
+    }).then((data) => {
       
-      $("#startButton").hide();
-    }});
-  });
+      trivia = console.log(JSON.stringify(data,null,3))
+      const questionNum = 1;
+      const score  = 0; 
+      $("#trueButton").show();
+      tbutton = $("#trueButton");
+$("#falseButton").show();
+fbutton = $("#falseButton") ;
+$("#restartButton").show();
+rbutton = $("#restartButton"); 
+$("#nextButton").show();
+nbutton = $("#nextButton");
+qSpace = $("#div1");
+scoreS = $("#score");
+
+$("#div1").html("Question " + questionNum + ": " + data.results[0].question);
+
+// Restart button function
+rbutton.click(function restart() {
+  const questionNum = 0;
+  const score = 0;
+  scoreS.html("");
+  fbutton.hide();
+  tbutton.hide();
+  nbutton.hide();
+  rbutton.hide();
+  qSpace.html("");
+  $("#questionButton").show();
+
+
 });
-$("#submitButton").show();
+
+  
 
 
-//for (i = 0; i < 10; i++) {
-  //$("#div1").html(result[i]);
+    }); 
+  })
+});
+
+
+
+
+
+
+
+
+
+//$("#startButton").click(function start() {
+//$("#startButton").hide();
+
+
+//$("#div2").html(data.question[0]);
+//});
+
+
+//$("#trueButton").click(function trueC() {
+  //if( data.correct_answer = "True") {
+    //questionNum += 1;
+    //score += 1;
+    //$("#score").html('Score: ' + score);
+  //}
+//}
+
+
+
+  //$("#startButton").click(function(foo){
+    //$.ajax({url: api, success: function(result){
+      //$("#div1").html(result);
+      //console.log(`result: ${JSON.stringify(result, null, 3)}`);
+      //const trivia = `result: ${JSON.stringify(result, null, 3)}`
+      
+      //$("#startButton").hide();
+      //$("#trueButton").show();
+      //$("#falseButton").show();
+      //$("#restartButton").show();
+      
+      //$("#question").val(triviaData);
+      
+
+   // }});
+ // });
+// });
+
+
+
+
+
+
 
   
     
